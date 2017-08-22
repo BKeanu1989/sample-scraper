b2e57833cd82a89e125e874a3747f342dba8ee7c
Kevin Fechner <bkevinfechner1989@gmail.com>
2017-08-22 17:03:31 +0200

    background processing wokring with ipc render message when successful; don't know what it I did wrong before ... must have been a typ fml

--- backgroundWorker.js
+++ backgroundWorker.js
@@ -1,10 +1,50 @@
 const fs = require('fs');
-var exec = require('child_process').exec;
+const util = require('util');
+// const exec = util.promisify(require('child_process').exec); // does not work with electron
+const exec = require('child_process').exec;
 
 function execute(command, callback){
     exec(command, function(error, stdout, stderr){ callback(stdout); });
 };
 
-execute('DEBUG=nightmare babel-node ./test-scraper.js', function(output) {
-    console.log(output);
-});
+
+// ipc.on('crawl-fb', function(event, fromWindowId) {
+//   execute('DEBUG=nightmare babel-node ./test-scraper.js', function(output) {
+//       console.log(output);
+//
+//   });
+//   const fromWindow = BrowserWindow.fromid(fromWindowId);
+//   fromWindow.webContents.send('fb-crawled');
+//   window.close();
+// });
+
+ipc.on('compute-factorial', function (event, number, fromWindowId) {
+    execute('DEBUG=nightmare babel-node ./test-scraper.js', function(output) {
+        // console.log(output);
+        console.log('fromWindowId:',fromWindowId);
+        const fromWindow = BrowserWindow.fromId(fromWindowId);
+        console.log(fromWindow);
+        console.log(fromWindow.webContents);
+        console.log(window);
+        fromWindow.webContents.send('test')
+        window.close();
+    });
+  // const result = factorial(number)
+  // const fromWindow = BrowserWindow.fromId(fromWindowId)
+  // fromWindow.webContents.send('factorial-computed', number, result)
+  // window.close()
+})
+
+function factorial (num) {
+  if (num === 0) return 1
+  return num * factorial(num - 1)
+}
+
+// async function executeScraper() {
+//   const {stdout, stderr} = await exec('node ./test-scraper.js');
+//   // const {stdout, stderr} = await exec('DEBUG=nightmare babel-node ./test-scraper.js');
+//   console.log('stdout:', stdout);
+//   console.log('stderr:', stderr);
+// }
+
+// executeScraper();
